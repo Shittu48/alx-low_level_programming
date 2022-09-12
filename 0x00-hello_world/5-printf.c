013a77896474ddebe9d07aa2f73331346a461dea
shittu48 <waleshittu48@gmail.com>
2022-09-12 15:49:52 -0700

    0-3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,6 @@
 /**
  * main - prints the string in the print function
  *
- *Description:  using the main function this program prints
- * with proper grammar, but the outcome is a piece of art
- *
  *Return: 0
  */
 int main(void)
