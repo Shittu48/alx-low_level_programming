43a04ba426a190117c0b06ac04b3ff2547d077f6
shittu48 <waleshittu48@gmail.com>
2022-09-11 20:59:37 -0700

    run

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - 
+ *
+ * Description: A program that prints 
+ * Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\nProgramming is like building a multilingual puzzle");
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main -
+ *
+ *Description:  a code that ptints
+ *  with proper grammar, but the outcome is a piece of art
+ *
+ *Return: Always 0 (Success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n")
+}
