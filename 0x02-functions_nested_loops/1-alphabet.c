cf57769f62310fef883c3ff864db55e593928547
shittu48 <waleshittu48@gmail.com>
2022-09-13 14:32:46 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - peint void 
+ *
+ * Returb: 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a', ch <= 'z', ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
