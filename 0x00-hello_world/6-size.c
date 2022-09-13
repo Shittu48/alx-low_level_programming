58fbaf044da1cc739805b6af31a888b35e0ef2b8
shittu48 <waleshittu48@gmail.com>
2022-09-12 20:30:54 -0700

    commit all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Print different sizes of variable
+ * main - print
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long: %d byte(s)\n", sizeof(d));
-	printf("Size of a float: %d byte(s)\n", sizeof(d));
+	printf("Size of a long long: %d bytes(s)\n", sizeof(d));
+	printf("Size of a float: %d bytes(s)\n", sizeof(e));
 	return (0);
 }
