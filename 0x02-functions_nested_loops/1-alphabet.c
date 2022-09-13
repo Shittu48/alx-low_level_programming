96364b99684ba2ddad7dfa3684f84cdc9929a6ea
shittu48 <waleshittu48@gmail.com>
2022-09-13 14:45:05 -0700

    now

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@ void print_alphabet(void)
 {
 	int ch;
 
-	for (ch = 'a', ch <= 'z', ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar('\n')
+	putchar('\n');
 int main(void)
 {
 	print_alphabet();
