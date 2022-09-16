b4387c2addc1623e0765b03220d55e3d7e88246b
shittu48 <waleshittu48@gmail.com>
2022-09-15 21:18:22 -0700

    Nested loops more functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,10 +11,10 @@ void print_number(int n)
 	unsigned int num = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		num = -num;
 	}
-	if (num > 9)
+	if (num / 10)
 	{
 		print_number(num / 10);
 	}
