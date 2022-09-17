49be1371fb1d959a17cad5e7938b849c2f4415ec
shittu48 <waleshittu48@gmail.com>
2022-09-16 18:11:55 -0700

    nest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,27 +16,23 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (num % 3 == 0)
-		{
-			printf("Fizz ");
-		}
 		else if (num % 5 == 0)
 		{
 			if (num == 100)
 			{
 				printf("Buzz ");
+				printf("\n");
 			}
 			else
 			{
 				printf("Buzz");
 			}
 		}
+		else if (num % 3 == 0)
+			printf("Fizz ");
 		else
-		{
 			printf("%d ", num);
-		}
-		num++;
+			num++;
 	}
-	printf("\n");
 	return (0);
 }
