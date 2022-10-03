4bc12bd4ba541909bf7d4d2ddcb24e43f53dcd5b
shittu48 <waleshittu48@gmail.com>
2022-10-03 12:10:39 -0700

    Arguments in C

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,14 +8,14 @@
  * @argv: arg vector
  * Return: 0 on Success and 1 when failed
  */
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 	int sum, i, j;
 
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
 			if (isdigit(argv[i][j] == 0))
 			{
