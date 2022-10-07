dba5b2a7aa768528fdcad3619f9d7af5bac60e1a
shittu48 <waleshittu48@gmail.com>
2022-10-06 18:08:30 -0700

    More on malloc, calloc and realloc in C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,6 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s3[i] = s1[i];
 	for (i = 0; i < n; i++)
 		s3[len1 + i] = s2[i];
-		s3[len1 + i] = '\0';
-		return (s3);
+	s3[len1 + i] = '\0';
+
+	return (s3);
 }
